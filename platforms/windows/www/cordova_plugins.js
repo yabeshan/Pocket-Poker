b508cfde019598197dd00c57a60a2ca40ef6393b
beshan <yabeshan@ya.ru>
2017-09-02 10:29:32 +0300

    init ionic3 angular4 added 4 platforms = android ios windowsphone browser

---
+++ platforms/windows/www/cordova_plugins.js
@@ -0,0 +1,84 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-console.logger",
+        "file": "plugins/cordova-plugin-console/www/logger.js",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "id": "cordova-plugin-console.console",
+        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "id": "cordova-plugin-device.DeviceProxy",
+        "file": "plugins/cordova-plugin-device/src/windows/DeviceProxy.js",
+        "pluginId": "cordova-plugin-device",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreenProxy",
+        "file": "plugins/cordova-plugin-splashscreen/www/windows/SplashScreenProxy.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "runs": true
+    },
+    {
+        "id": "cordova-plugin-statusbar.statusbar",
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "id": "cordova-plugin-statusbar.StatusBarProxy",
+        "file": "plugins/cordova-plugin-statusbar/src/windows/StatusBarProxy.js",
+        "pluginId": "cordova-plugin-statusbar",
+        "runs": true
+    },
+    {
+        "id": "ionic-plugin-keyboard.KeyboardProxy",
+        "file": "plugins/ionic-plugin-keyboard/src/windows/KeyboardProxy.js",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-console": "1.0.5",
+    "cordova-plugin-device": "1.1.4",
+    "cordova-plugin-splashscreen": "4.0.3",
+    "cordova-plugin-statusbar": "2.2.2",
+    "cordova-plugin-whitelist": "1.3.1",
+    "ionic-plugin-keyboard": "2.2.1"
+};
+// BOTTOM OF METADATA
+});
---
+++ platforms/windows/www/service-worker.js
@@ -0,0 +1,11 @@
+﻿self.addEventListener('activate', function (event) {
+
+});
+
+self.addEventListener('fetch', function (event) {
+
+});
+
+self.addEventListener('push', function (event) {
+
+});
