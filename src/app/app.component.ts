e76505432818d88ba0660d1edc4e2686a7dac7d0
beshan <yabeshan@ya.ru>
2017-09-02 22:16:44 +0300

    first build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,11 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      // statusBar.styleDefault();
+      setTimeout(()=>{
+        splashScreen.hide();
+      },15000);
+      
     });
   }
 }
